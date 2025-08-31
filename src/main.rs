cac8de1c79f5f6b59077e06fb9b8ff8102f951e5
zowie <git@zowie.cx>
2025-08-31 20:01:23 +0200

    feat: config file generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
 use color_eyre::eyre::Result;
+use eyre::eyre;
+use std::fs;
 
-use crate::config::{CommandLine, Config};
+use crate::config::{Command, CommandLine, Config};
 
 mod config;
 
@@ -9,6 +11,20 @@ fn main() -> Result<()> {
     color_eyre::install()?;
 
     let cli = CommandLine::parse();
+    match &cli.command {
+        Command::Config => {
+            if fs::exists(&cli.config)? {
+                return Err(eyre!("ERROR: config file {} already exists", cli.config));
+            }
+
+            let cfg = Config::default();
+            let serialized = toml::to_string_pretty(&cfg)?;
+            fs::write(&cli.config, serialized)?;
+        }
+        Command::Sync { execute } => {
+            todo!()
+        }
+    }
     let conf = Config::load(cli)?;
 
     Ok(())
