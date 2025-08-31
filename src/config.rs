51775e7b426e70f98ca295bbb2811968901e3a17
zowie <git@zowie.cx>
2025-08-31 19:37:32 +0200

    feat: implement defaults for config

--- src/config.rs
+++ src/config.rs
@@ -26,11 +26,33 @@ pub(crate) struct NameConfig {
     display_name_pronouns: bool,
 }
 
+#[expect(
+    clippy::derivable_impls,
+    reason = "explicit is better, expect more flags in the future"
+)]
+impl Default for NameConfig {
+    fn default() -> Self {
+        Self {
+            display_name_pronouns: false,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct Config {
     scan_type: ScanConfig,
     token: String,
     name: NameConfig,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            token: String::from("YOUR_PK_TOKEN"),
+            scan_type: ScanConfig::default(),
+            name: NameConfig::default(),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -40,6 +62,12 @@ enum ScanConfig {
     Path(PathScanConfig),
 }
 
+impl Default for ScanConfig {
+    fn default() -> Self {
+        Self::Path(PathScanConfig::default())
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct TagScanConfig {
     root_dir: String,
@@ -47,11 +75,37 @@ pub(crate) struct TagScanConfig {
     group_tags: Option<Vec<String>>,
 }
 
+impl Default for TagScanConfig {
+    fn default() -> Self {
+        Self {
+            root_dir: String::from("~/notes/system"),
+            member_tags: Some(vec![
+                String::from("#plurality"),
+                String::from("#system/member"),
+            ]),
+            group_tags: Some(vec![
+                String::from("#plurality"),
+                String::from("#system/group"),
+            ]),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct PathScanConfig {
     recursive: bool,
     member_dir: Option<String>,
     group_dir: Option<String>,
+}
+
+impl Default for PathScanConfig {
+    fn default() -> Self {
+        Self {
+            recursive: false,
+            member_dir: Some(String::from("~/notes/system/members")),
+            group_dir: Some(String::from("~/notes/system/groups")),
+        }
+    }
 }
 
 impl Config {
